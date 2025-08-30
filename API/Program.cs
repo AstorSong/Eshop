9cfb35b1310b0c0bfa1a550c0db294c00beef1ac
Zhaoyi (Astor) Song <ab13522095821@gmail.com>
2025-08-29 22:57:00 -0400

    Added data seeding logic to ensure the database is up-to-date and has readily available data.

--- API/Program.cs
+++ API/Program.cs
@@ -29,4 +29,21 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+try
+{
+    // Ensures the scope (and its services) are disposed of properly after use.
+    using var scope = app.Services.CreateScope();
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<StoreContext>();
+    // Applies any pending migrations to the database.
+    // Equivalent to running dotnet ef databse update, but done at runtime.
+    await context.Database.MigrateAsync();
+    await StoreContextSeed.SeedAsync(context);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex);
+    throw;
+}
+
 app.Run();
